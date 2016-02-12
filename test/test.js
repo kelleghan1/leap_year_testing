0c8f0fd48e805756fc99d8ad857ab58a99069af1
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-12 12:02:41 -0700

    Added multiple tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
 var code = require('../main');
 var expect = require('chai').expect;
-
 describe('Is a leap year', function(){
-  it('Whether leap year or not', function () {
+  it('Whether leap year or not', function(){
     expect(code.leapYear(2016)).to.equal(true)
+  })
+  it('Whether leap year or not', function(){
+    expect(code.leapYear(1600)).to.equal(true)
+  })
+  it('Whether leap year or not', function(){
+    expect(code.leapYear(2000)).to.equal(true)
+  })
+  it('Whether leap year or not', function(){
+    expect(code.leapYear(1700)).to.equal(false)
+  })
+  it('Whether leap year or not', function(){
+    expect(code.leapYear(1800)).to.equal(false)
+  })
+  it('Whether leap year or not', function(){
+    expect(code.leapYear(1900)).to.equal(false)
   })
 });
