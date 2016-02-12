373ce2597731e22b646fddb1b1c2d02a0245f6ea
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-11 21:57:44 -0700

    Leap year function complete

---
+++ main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  leapYear: function (thisYear) {
+
+    if( ((thisYear % 4 == 0) && (thisYear % 100 !== 0)) || (thisYear % 400 == 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
