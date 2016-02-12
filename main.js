ee71635562ce78c4953b571f5cd5f44ce94344c9
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-12 08:58:09 -0700

    Syntax styling

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 module.exports = {
-  leapYear: function (thisYear) {
-
-    if( ((thisYear % 4 == 0) && (thisYear % 100 !== 0)) || (thisYear % 400 == 0)) {
+  leapYear:function(thisYear){
+    if(((thisYear % 4 == 0)&&(thisYear % 100 !== 0))||(thisYear % 400 == 0)){
       return true;
-    } else {
+    }else{
       return false;
     }
   }
